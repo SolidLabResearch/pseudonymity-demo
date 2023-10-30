62f5657d928ea235c767965e04458715c8e06bd8
Gertjan De Mulder <gertjan.demulder@ugent.be>
2023-10-30 11:25:36 +0100

    added CssUserConfig interface

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -2,3 +2,14 @@ export interface ClientCredentials {
   id: string;
   secret: string;
 }
+
+export interface CssUserConfig {
+  podName: string
+  email: string
+  password: string
+  createWebId: boolean
+  createPod: boolean
+  register: boolean
+  webId: string
+  css: string
+}
