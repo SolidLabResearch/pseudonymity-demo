1078cad3e4a6572c815f20d835336e514db37b21
Gertjan De Mulder <gertjan.demuder@ugent.be>
2023-11-04 21:36:00 +0100

    add: _hack_addEnsureContextFunction

--- src/util.ts
+++ src/util.ts
@@ -140,3 +140,27 @@ export async function registerUsersAndPods(users: any): Promise<Record<string, a
   }
   return userOnControls;
 }
+
+export function _hack_addEnsureContextFunction(suite: any) {
+  suite.ensureSuiteContext = ({ document }: any) => {
+    const contextUrls = [
+      // 'https://w3id.org/security/suites/bls12381-2020/v1',
+      'https://w3id.org/security/bbs/v1'
+    ];
+
+    if (typeof document['@context'] === 'string' && contextUrls.includes(document['@context'])) {
+      return;
+    }
+
+    if (Array.isArray(document['@context']) &&
+        contextUrls.filter(url => document['@context'].includes(url)).length) {
+      return;
+    }
+
+    throw new TypeError(
+        `The document to be signed must contain one of this suite's @context, ` +
+        `"${contextUrls.join(', ')}", got "${document['@context']?.join(', ')}".`
+    );
+  };
+  return suite;
+}
