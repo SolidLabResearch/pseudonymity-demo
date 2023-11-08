60184e2ee936fc2b2b9622695f5527c9be165b92
Gertjan De Mulder <gertjan.demulder@ugent.be>
2023-11-08 13:50:45 +0100

    deleted transfer func

--- src/setup.ts
+++ src/setup.ts
@@ -8,6 +8,8 @@ import fetch from "cross-fetch";
 import {logger} from "./logger";
 import {CssProxy} from "./components/anonymizer/CssProxy";
 import { fileTypeFromFile } from 'file-type';
+import {SolidVCActor} from "./components/solid-actor/SolidVCActor";
+import {createCustomDocumentLoader, ctx} from "./contexts/contexts";
 
 
 const users: Array<CssUserConfig> = readJsonFile('./common/css-users.json')
@@ -23,7 +25,11 @@ async function preflight() {
   } else {
     userOnControls = readJsonFile('./dev.user-on-controls.json')
   }
-  return userOnControls;
+  const fpathUsersAndCredentials = 'usersAndClientCredentials.json';
+  if(!fs.existsSync(fpathUsersAndCredentials)) {
+      writeJsonFile(fpathUsersAndCredentials, await getUserCredentials(userOnControls));
+  }
+  return readJsonFile(fpathUsersAndCredentials)
 }
 
 async function getUserCredentials(userOnControls: Record<string, any>) {
@@ -46,103 +52,24 @@ async function getUserCredentials(userOnControls: Record<string, any>) {
         }]
       }))
   )
+
     return usersAndCredentials
 }
 
-async function transfer(rootDir: string, parentDir: string, _fetch: typeof fetch, controls:any) {
-    const relDir = parentDir.replace(rootDir, '')
-    const dirents =  fs.readdirSync(parentDir, {withFileTypes: true})
-    // Transfer directories
-    const dirs = dirents.filter(x => x.isDirectory())
-    for await (const srcDir of dirs) {
-        logger.info(`Transfer subdir: ${srcDir.name}`)
-        const pathContainer = path.join(relDir, srcDir.name) + '/'
-        const dstContainer = path.join(controls.pod, pathContainer)
-
-        const containerExists = (await _fetch(dstContainer, { method: 'GET'})).status === 200
-        if(!containerExists) {
-            // Create container for subdir
-            const container = await createContainerAt(dstContainer, {fetch: _fetch})
-            logger.info('Created container:\n', container)
-        } else {
-            logger.info(`Container ${dstContainer} already exists!`)
-        }
-
-        // Transfer files within subdir
-        await transfer(rootDir, path.join(parentDir, srcDir.name), _fetch, controls)
-    }
-
-    // Transfer files
-    const srcFiles = dirents
-        .filter(x => x.isFile())
-
-    for await (const src of srcFiles) {
-
-        const srcPath = path.join(parentDir, src.name);
-
-        const extOnMime = new Map(Object.entries({
-            'json': 'application/json',
-            'jsonld': 'application/ld+json',
-            'ttl': 'text/turtle',
-        }))
-
-        const [basename, ext] = src.name.split('.').slice(-2)
-        const mt = extOnMime.get(ext)
-
-        if(!mt) {
-            throw new Error(`Cannot find mime-type for extension: ${ext} (file: ${src.name})`)
-        }
-
-        const srcData = fs.readFileSync(srcPath, { encoding: 'utf-8' })
-        const dst = path.join(controls.pod, relDir, src.name)
-        const {status :probeStatus} = await _fetch(dst)
-        logger.debug(`💉 Probe: ${dst} - Status: ${probeStatus}`)
-
-        // const method = probeStatus === 404 ? 'POST' : 'PUT';
-        const method = 'PUT';
-        // Request parameters, except for the body
-        const requestParams = {
-            method: method,
-            headers: { 'content-type': mt!, 'Slug': basename},
-        }
-        logger.debug(`>> ${method} - ${JSON.stringify(requestParams, null, 2)}`)
-        const response = await _fetch(dst, {
-            ...requestParams,
-            body: srcData
-        });
-
-        if(!response.ok) {
-            logger.error(`❌FAILED: ${method} - Status: ${response.status} - Status text: ${response.statusText}`)
-        } else {
-            const payload = await response.text();
-            logger.debug(`✅ ${method} - Status: ${response.status} - Status text: ${response.statusText} - ${payload}`)
-        }
-    }
-}
-
 preflight()
-  .then(getUserCredentials)
-    .then(usersAndCredentials => {
-        writeJsonFile('usersAndClientCredentials.json', usersAndCredentials);
-        return usersAndCredentials
-    })
-    // .then((usersAndCredentials) => {
-    //     // TODO: TEMPORARY DEV SETTING
-    //     return Object.fromEntries(Object.entries(usersAndCredentials).filter(([email, other])=>email === 'recruiter@hr.com'))
-    // })
-    .then(async (usersAndCredentials)=> {
-        logger.info('Loading intial data on pods')
+    .then(async (usersAndCredentials): Promise<SolidVCActor[]> => {
+        return Object.entries(usersAndCredentials)
+            .map(([email,actorParams])=> {
+                const { user: { webId }, credentials, controls} = (actorParams as any)!
+                const proxy = new CssProxy(
+                    credentials.clientCredentials,
+                    webId,
+                    controls
+                )
 
-        // load initial data on pods
-        for await (const [email, userObject] of Object.entries(usersAndCredentials)) {
-            const {credentials: { clientCredentials}, controls, user: { podName, webId}} = (userObject as any)
-            const cssProxy = new CssProxy(clientCredentials, webId, controls)
-            await cssProxy.intializeFetch();
-            const pathActorDir = path.resolve(path.join('./actors', podName))
-            // Initiate transfer
-            const rootDir = pathActorDir
-            const parentDir = pathActorDir
-            await transfer(rootDir, parentDir, cssProxy.fetch!, controls)
-        }
+                const dl = createCustomDocumentLoader(ctx)
+                const a = new SolidVCActor(proxy, webId, dl)
+                return a
+            })
     })
   .then(() => console.log('Done'))
