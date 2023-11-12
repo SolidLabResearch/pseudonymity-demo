fc05015ab2a752bb3a35342c1c793b4e85fbb6f4
Gertjan De Mulder <gertjan.demulder@ugent.be>
2023-11-12 02:42:08 +0100

    updated jest reporting

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,28 @@
 module.exports = {
     collectCoverage: true,
     coverageReporters: ['clover', 'json', 'lcov', ['text', {skipFull: true}]],
+    coveragePathIgnorePatterns: [
+        '/dist/',
+        '/build/',
+        '/node_modules/',
+        '/test/',
+    ],
+    coverageThreshold: {
+        './src': {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100,
+        },
+    },
     transform: {
         '^.+\\.ts$': ['ts-jest', {
             tsconfig: 'tsconfig.json',
         }],
     },
+    reporters: [
+        'default',
+    ],
     // Only run tests in the unit and integration folders.
     // All test files need to have the suffix `.test.ts`.
     testRegex: 'src/tests/(components|usecases)/.*\\.test\\.ts$',
