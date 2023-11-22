d5d1f6670484a94b0f90bc558f99bf1238f41b84
Gertjan De Mulder <gertjan.demuder@ugent.be>
2023-11-22 01:04:41 +0100

    update: profiling scripts

--- profiler.sh
+++ profiler.sh
@@ -2,29 +2,16 @@ set -e
 set -u
 
 DIR_PROFILING=$(PWD)/reports/profiling
-FPATH_EVALUATOR=$(PWD)/build/profiling/evaluator.js
 mkdir -p $DIR_PROFILING
 
 N=100
+DT_START=$(date)
 
-function profileWebResolvable() {
-    echo "Profile: Web-resolvable DID Document"
-    for i in $(seq 1 $N); do
-      npm run test:components:setup
-      npm run profile:web-resolvable
-    done
-}
+for i in $(seq 1 $N); do
+    npm run profile
+done
 
+DT_END=$(date)
 
-
-# Evaluation: did-key
-function profileDidKey() {
-  echo "Profile: DID Key Document"
-  for i in $(seq 1 $N); do
-    npm run profile:did-key
-  done
-}
-
-
-profileWebResolvable
-profileDidKey
+echo "Started: $DT_START"
+echo "Ended: $DT_END"
