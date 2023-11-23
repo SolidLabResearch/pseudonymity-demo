fff30be18b464284fa54c843a1079a6ecb4edda8
Gertjan De Mulder <gertjan.demuder@ugent.be>
2023-11-23 01:44:08 +0100

    Clean up: SolidVCActorFactory now takes care of registration

--- src/tests/components/SolidVCActor.test.ts
+++ src/tests/components/SolidVCActor.test.ts
@@ -13,24 +13,7 @@ describe('SolidVCActor', (): void => {
     const SELECTED_TEST_ACTOR = 'alice'
 
     let records: Array<ITestRecord> = cssTestConfigRecords.filter(r => r.testConfig.name === SELECTED_TEST_ACTOR)
-    const actorFactory = new SolidVCActorFactory()
-
-    beforeAll(async (): Promise<void> => {
-        // Create & start each actor's app (server)
-        for await (const r of records) {
-            // Register users & pods, and get each actor's controls object
-            const controls = await register(r.userConfig)
-            expect(controls).toBeTruthy()
-            r.controls = controls
-            // Obtain client credentials
-            r.clientCredentials = await obtainClientCredentials(r.userConfig, r.controls!)
-        }
-    });
-
-    afterAll(async (): Promise<void> => {
-
-    });
-
+    const actorFactory = new SolidVCActorFactory(true)
 
     async function createInitializedActor(r: ITestRecord): Promise<SolidVCActor> {
         return await actorFactory.createInitializedActor(r)
