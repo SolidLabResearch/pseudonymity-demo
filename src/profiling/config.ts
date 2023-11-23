8567f43974cc54f04ee76273dc6788baab52d995
Gertjan De Mulder <gertjan.demuder@ugent.be>
2023-11-23 02:19:10 +0100

    Introduced enum for configuring profile mode (single/multi-actor)

--- src/profiling/config.ts
+++ src/profiling/config.ts
@@ -2,3 +2,10 @@ import path from "path";
 
 const dirReports = path.resolve(__dirname, '../../reports/')
 export const dirProfilingReports = path.join(dirReports, 'profiling')
+
+export enum ProfileMode {
+    singleActor,
+    multiActor
+
+}
+export const profileMode : ProfileMode = ProfileMode.multiActor
