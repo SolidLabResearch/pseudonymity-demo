92c11cbaddb2b0fee6f9b5e2c0559b23e6a69b00
Gertjan De Mulder <gertjan.demuder@ugent.be>
2023-11-23 02:22:33 +0100

    now also multi-actor profile mode enabled

--- src/profiling/web-resolvable.ts
+++ src/profiling/web-resolvable.ts
@@ -1,27 +1,24 @@
-import {createCustomDocumentLoader, ctx} from "../contexts/contexts";
 import {ITestRecord} from "../tests/interfaces";
-import {CssProxy} from "../components/solid-actor/CssProxy";
 import {cssTestConfigRecords} from "../tests/config/actorsOnCssTestConfigs";
-import {obtainClientCredentials, register} from "../utils/css";
 import {runEvaluation} from "./evaluator";
-import path from "path";
-import {Bls12381G2KeyPair} from "@mattrglobal/jsonld-signatures-bbs";
-import {SolidVCActor} from "../components/solid-actor/SolidVCActor";
-import {joinUrlPaths} from "../utils/url";
 import {SolidVCActorFactory} from "../tests/ActorFactory";
+import {ProfileMode, profileMode} from "./config";
+import {runMultiActorEvaluation} from "./multi-actor";
 
-export async function createInitializedActor(): Promise<SolidVCActor> {
-    const r = cssTestConfigRecords.find(r => r.testConfig.name === 'alice')! as ITestRecord
-    // Build record
-    // Register users & pods, and get each actor's controls object
-    r.controls = await register(r.userConfig)
-    // Obtain client credentials
-    r.clientCredentials = await obtainClientCredentials(r.userConfig, r.controls!)
-    const actorFactory = new SolidVCActorFactory()
-    return await actorFactory.createInitializedActor(r)
+const actorFactory = new SolidVCActorFactory(true)
+switch (profileMode) {
+    case ProfileMode.singleActor:
+        const r = cssTestConfigRecords.find(r => r.testConfig.name === 'alice')! as ITestRecord
+        actorFactory.createInitializedActor(r)
+            .then(runEvaluation)
+            .then()
+            .catch(console.error)
+        break
+    case ProfileMode.multiActor:
+        runMultiActorEvaluation(actorFactory)
+            .then()
+            .catch(console.error)
+        break;
+    default:
+        throw new Error('Unknown Profile Mode')
 }
-
-createInitializedActor()
-    .then(runEvaluation)
-    .then()
-    .catch(console.error)
