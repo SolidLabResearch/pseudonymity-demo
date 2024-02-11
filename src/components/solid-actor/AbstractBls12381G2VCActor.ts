ff0b0cd83ed32181cf63e6aa0cf3d5308997a2be
Gertjan De Mulder <gertjan.demuder@ugent.be>
2024-02-11 22:36:32 +0100

    Deleted KeyPairActor

--- src/components/solid-actor/AbstractBls12381G2VCActor.ts
+++ src/components/solid-actor/AbstractBls12381G2VCActor.ts
@@ -1,13 +1,12 @@
 import {GenericVCActor} from "./GenericVCActor";
 import {BbsBlsSignature2020, BbsBlsSignatureProof2020, Bls12381G2KeyPair} from "@mattrglobal/jsonld-signatures-bbs";
-import {KeyPairActor} from "./KeyPairActor";
 import {_hack_addEnsureContextFunction} from "../../utils/cryptosuite";
 import {ICredentialActor, IDidDocument} from "../interfaces";
 import {IDocumentLoader} from "../../interfaces";
 
 export abstract class AbstractBls12381G2VCActor
     extends GenericVCActor<BbsBlsSignature2020, BbsBlsSignature2020, BbsBlsSignatureProof2020>
-    implements ICredentialActor, KeyPairActor<Bls12381G2KeyPair> {
+    implements ICredentialActor {
     key: Bls12381G2KeyPair;
     fingerprint?: string
     _controllerDocument: IDidDocument
